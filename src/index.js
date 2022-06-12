1b1afa29c4b0132c4784951490696dec87682a26
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-06-12 22:43:20 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './css/styles.css';
 import { fetchCountries } from "./js/fetchCountries.js";
-const _= require('lodash');
+const _ = require('lodash');
+import Notiflix from 'notiflix';
+// import 'flatpickr/dist/flatpickr.min.css';
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector("#search-box");
 const countryList = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
 
-
+let width = "30px"
 input.style.borderWidth = "5px";
 input.style.maxWidth = "500px";
 input.style.minWidth = "300px";
@@ -16,27 +18,80 @@ input.addEventListener("focus", (event) => {
     event.target.style.borderColor = "lightblue";
 });
 
+countryList.style.listStyle = "none";
+countryList.style.fontSize = "20px";
+countryList.style.lineHeight = "3";
+countryList.style.verticalAlign = "middle";
 
-input.addEventListener("input",_.debounce((event) => {
-    fetchCountries(event.target.value.trim()).then((arrayOfCountries) =>
-        renderArrayOfCountries(arrayOfCountries))},
+input.addEventListener("input", _.debounce((event) => {
+    fetchCountries(event.target.value.trim())
+      .then((arrayOfCountries) =>
+        renderArrayOfCountries(arrayOfCountries))
+      .catch((error) => console.log(error));
+    },
     DEBOUNCE_DELAY,
     {
-        leading: false,
-        trailing: true,
-    }));
-
-
-// fetchCountries().then((arrayOfCountries) => console.log(arrayOfCountries));
-// fetchCountries().then((arrayOfCountries) => renderArrayOfCountries(arrayOfCountries));
+      leading: false,
+      trailing: true,
+    }
+  )
+);
 
 function renderArrayOfCountries(arrayOfCountries) {
-      const markup = arrayOfCountries
+  let numberOfCountries = arrayOfCountries.length;
+  console.log(numberOfCountries);
+  if (numberOfCountries>10) {
+    moreThan10(arrayOfCountries)
+  };
+  if (numberOfCountries<11 && numberOfCountries > 1) {
+    showCountries(arrayOfCountries);
+  };
+  if (numberOfCountries === 1) {
+    show1Country(arrayOfCountries)
+  };  
+};
+
+function moreThan10(arrayOfCountries) {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+    let markup = "";
+    countryList.innerHTML = markup;
+}
+
+function showCountries(arrayOfCountries) {
+    show(arrayOfCountries);
+}
+
+function show1Country(arrayOfCountries) {
+    show(arrayOfCountries);
+    showCountryInfo(arrayOfCountries);
+}
+
+function show(arrayOfCountries) {
+  width = "30px";
+  const markup = arrayOfCountries
     .map((country) => {
       return `<li>
-          <p>${country.name.common}</p>
-        </li>`;
+      <img src=${country.flags.svg} alt=${country.name.common} width="${width}" heigth="${width}">
+      <b>${country.name.common}</b>
+    </li>`;
     })
     .join("");
   countryList.innerHTML = markup;
+  countryInfo.innerHTML = "";
+};
+
+function showCountryInfo(arrayOfCountries) {
+  width = "50px";
+  const markup = arrayOfCountries
+              .map((country) => {
+                let languages = Object.values(country.languages);
+                countryList.style.fontSize = "30px";
+          return `
+          <p>Capital: ${country.capital}</p>
+          <p>Population ${country.population}</p>
+          <p>Languages: ${languages}</p>
+        `;
+        })
+        .join("");
+      countryInfo.innerHTML = markup;
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,14 @@
 function fetchCountries(name) {
     const searchParams = new URLSearchParams({
         fields:"name,capital,population,flags,languages" 
-
     });
     return fetch(`https://restcountries.com/v3.1/name/${name}?${searchParams}`)
         .then((response) => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
-        return response.json();
-    });
+            return response.json();
+        })
+        .catch((error) => console.log(error));
 }
 export { fetchCountries };
